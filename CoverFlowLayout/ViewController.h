952541c8fdddab704f65e2836a4f7967337027a6
ttjnguyen <ttjnguyen6@gmail.com>
2016-11-18 06:03:47 -0800

    Initial Commit

---
+++ CoverFlowLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoverFlowLayout
+//
+//  Created by Jenny Nguyen on 2016-11-18.
+//  Copyright © 2016 LightHouseLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
